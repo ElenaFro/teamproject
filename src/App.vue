1714ba3106474ec4323a3ba95cda94e96151629f
Trifonix <trifonix@rocketmail.com>
2024-04-30 20:54:11 +0300

    init start page

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,21 @@
-<template><p>Hello, 1T!</p></template>
+<template>
+  <article class="message">
+    <div class="message-header">
+      <p>Plants shop</p>
+      <button class="delete" aria-label="delete"></button>
+    </div>
+    <div class="message-body">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec
+      nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus
+      diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac
+      <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et
+      sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a
+      neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+    </div>
+  </article>
+</template>
+
+<style>
+@import "bulma/css/bulma.min.css";
+</style>
