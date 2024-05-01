1174c3088663958c2704969ed84f2d198d25a50f
Elena Frolova <frolova.est@gmail.com>
2024-05-01 10:16:57 +0700

    Добавила главную MainPage и компоненты

---
+++ src/components/MainPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <Header />
+    <Navigation />
+    <About />
+    <ProductCatalog />
+    <News />
+    <Advertisement />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Navigation from './Navigation.vue';
+import About from './About.vue';
+import ProductCatalog from './ProductCatalog.vue';
+import News from './News.vue';
+import Advertisement from './Advertisement.vue';
+import Footer from './Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Navigation,
+    About,
+    ProductCatalog,
+    News,
+    Advertisement,
+    Footer,
+  },
+};
+</script>
---
+++ src/components/ProductCatalog.vue
@@ -0,0 +1,20 @@
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const products = ref([]);
+
+    // Fetch products data from an API
+    const fetchProducts = async () => {
+      // Make API call to fetch products
+      // Update products value
+    };
+
+    // Call fetchProducts function when the component is mounted
+    fetchProducts();
+
+    return {
+      products,
+    };
+  },
+};
