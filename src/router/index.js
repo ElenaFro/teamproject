6e4009469c5d01e5bfc50a602efb2109612e09cc
Trifonix <trifonix@rocketmail.com>
2024-05-06 01:17:42 +0300

    add Catalog route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import StartPage from "/src/components/StartPage.vue";
 import Main from "/src/components/MainPage.vue";
 import NotFound from "/src/components/NotFoundPage.vue";
+import Catalog from "/src/components/CardCatalog.vue";
 
 export function isAuth(email, pass) {
   return email === "1t@1t.ru" && pass === "1t";
@@ -17,6 +18,11 @@ const routes = [
     path: "/main",
     name: "main",
     component: Main,
+  },
+  {
+    path: "/catalog",
+    name: "catalog",
+    component: Catalog,
   },
   {
     path: "/:pathMatch(.*)*",
