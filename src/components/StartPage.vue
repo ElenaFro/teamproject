bd6e6f2c26c9b303683d126b1701a8bdcb5e333b
Trifonix <trifonix@rocketmail.com>
2024-05-05 17:58:39 +0300

    fix start page

--- src/components/StartPage.vue
+++ src/components/StartPage.vue
@@ -1,64 +1,74 @@
 <template>
-  <div class='start-wrapper'>
-    <section class="hero  hero1 is-fullheight" style="background-image: url('src/assets/fon.jpg'); background-size: center; background-position: left;">
+  <div class="start-wrapper">
+    <section
+      class="hero hero1 is-fullheight"
+      style="
+        background-image: url('src/assets/fon.jpg');
+        background-size: center;
+        background-position: left;
+      "
+    >
       <div class="hero-body">
         <article class="message is-success">
-            <div class="message-header">
-                <p>Добро пожаловать в наш уютный магазин растений, друзья!</p>
-            </div>
-            <div class="message-body">
-                Здесь вы найдете не только зелень для вашего дома или офиса, но и
-                вдохновение для создания живых оазисов вокруг себя. Мы стремимся
-                предложить вам <strong>самые красивые и здоровые растения</strong>. От
-                зеленых друзей для ваших оконных подоконников до величественных деревьев
-                для вашего сада — у нас есть что-то <em>для каждого любителя природы</em>.
-                Наша команда с удовольствием поможет вам сделать выбор и обеспечить уход
-                за вашими новыми зелеными подопечными.
-            </div>
+          <div class="message-header">
+            <p>Добро пожаловать в наш уютный магазин растений</p>
+          </div>
+          <div class="message-body">
+            Здесь вы найдете не только зелень для вашего дома или офиса, но и
+            вдохновение для создания живых оазисов вокруг себя. Мы стремимся
+            предложить вам <strong>самые красивые и здоровые растения</strong>.
+            От зеленых друзей для ваших оконных подоконников до величественных
+            деревьев для вашего сада — у нас есть что-то
+            <em>для каждого любителя природы</em>. Наша команда с удовольствием
+            поможет вам сделать выбор и обеспечить уход за вашими новыми
+            зелеными подопечными.
+          </div>
         </article>
       </div>
-   </section>
+    </section>
 
-   <form class="box-form ml-6 pl-6 mt-6">
+    <form class="box-form ml-6 pl-6 mt-6">
       <figure class="image is-200x200 mx-auto mb-6 mt-6">
-        <img src="/src/assets/logo-start.png"/>
+        <img src="/src/assets/logo-start.png" />
       </figure>
-        <h2 class="mb-4 is-size-4 mt-6">Вход / Регистрация</h2>
-        <div class="field">
-            <label class="label">Email</label>
-            <div class="control">
-              <input
-                class="input"
-                type="email"
-                name="email"
-                v-model="email"
-                placeholder="1t"
-              />
-            </div>
+      <h2 class="mb-4 is-size-4 mt-6">Вход / Регистрация</h2>
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input
+            class="input"
+            type="email"
+            name="email"
+            v-model="email"
+            placeholder="1t"
+          />
         </div>
+      </div>
 
-        <div class="field">
-            <label class="label">Пароль</label>
-            <div class="control">
-              <input
-                class="input"
-                type="password"
-                name="pswd"
-                v-model="pass"
-                placeholder="1t"
-              />
-            </div>
+      <div class="field">
+        <label class="label">Пароль</label>
+        <div class="control">
+          <input
+            class="input"
+            type="password"
+            name="pswd"
+            v-model="pass"
+            placeholder="1t"
+          />
         </div>
+      </div>
 
-        <div>
-            <a href="http://">Забыли пароль?</a>
-        </div>
-        <button class="button is-success mb-4 mr-3 mt-4" @click="submit()">Войти</button>
-        <button class="button is-success is-outlined mt-4">Зарегистрироваться</button>
+      <div>
+        <a href="http://">Забыли пароль?</a>
+      </div>
+      <button class="button is-success mb-4 mr-3 mt-4" @click="submit">
+        Войти
+      </button>
+      <button class="button is-success is-outlined mt-4">
+        Зарегистрироваться
+      </button>
     </form>
-
   </div>
-    
 </template>
 
 <script>
@@ -106,26 +116,25 @@ section {
 .start-wrapper {
   display: flex;
   justify-content: space-between;
-
 }
 
 .message {
   margin: 60px;
 }
 
-.hero1{
-    width: 55%;
+.hero1 {
+  width: 55%;
 }
 
-.box-form {
-    /* width: 700px;
-    height: 300px; */
-    /* display: flex;
-    flex-direction: column;
-    justify-content: top; */
-    /* margin-right: 300px; */
+/* .box-form {
+  width: 700px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: top;
+  margin-right: 300px;
 }
-/* 
+
 #app {
   padding-right: 2rem;
   padding-left: 0;
