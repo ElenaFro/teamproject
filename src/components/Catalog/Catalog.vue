3b1519db21dcb8436c1caa7eab328ff0d781beb2
Trifonix <trifonix@rocketmail.com>
2024-05-08 19:30:54 +0300

    fix Catalog option title

--- src/components/Catalog/Catalog.vue
+++ src/components/Catalog/Catalog.vue
@@ -1,7 +1,5 @@
 <template>
-
-  <div class="catalog-filters  mt-6 ml-6 mb-5">
-
+  <div class="catalog-filters mt-6 ml-6 mb-5">
     <div class="select is-small mr-3">
       <select v-model="filterOptions.category1">
         <option value="">Все категории</option>
@@ -22,12 +20,12 @@
     <div class="select is-small mr-4">
       <select v-model="filterOptions.availability">
         <option value="">Все (наличие)</option>
-        <option value="true">В наличиии</option>
+        <option value="true">В наличии</option>
         <option value="false">Нет в наличии</option>
       </select>
     </div>
 
-    <input type="checkbox" v-model="filterOptions.sale"> Со скидкой
+    <input type="checkbox" v-model="filterOptions.sale" /> Со скидкой
   </div>
 
   <!-- <div class="catalog">
@@ -35,9 +33,13 @@
   </div> -->
 
   <div class="catalog">
-    <CatalogItem v-for="plant in filteredPlants" :key="plant.vcode" :plant_data="plant" @addToCart="addToCart" />
+    <CatalogItem
+      v-for="plant in filteredPlants"
+      :key="plant.vcode"
+      :plant_data="plant"
+      @addToCart="addToCart"
+    />
   </div>
-
 </template>
 
 <script>
@@ -55,21 +57,21 @@ export default {
         // category1: "Цветы"
         category2: "",
         availability: "",
-        sale: false
-      }    
+        sale: false,
+      },
     };
   },
   watch: {
     filterOptions: {
       handler: "filterPlants",
-      deep: true
-    }
+      deep: true,
+    },
   },
   computed: {
     ...mapGetters(["PLANTS"]),
     filteredPlants() {
-    return this.filterPlants();
-  }
+      return this.filterPlants();
+    },
   },
   methods: {
     ...mapActions(["GET_PLANTS_FROM_API", "ADD_TO_CART"]),
@@ -78,14 +80,21 @@ export default {
       // this.$router.push({ name: 'CartPage' }); // Переход на страницу CartPage
     },
     filterPlants() {
-      return this.PLANTS.filter(plant => {
-        let passCategory1 = this.filterOptions.category1 === "" || plant.category1 === this.filterOptions.category1;
-        let passCategory2 = this.filterOptions.category2 === "" || plant.category2 === this.filterOptions.category2;
-        let passAvailability = this.filterOptions.availability === "" || plant.availability.toString() === this.filterOptions.availability;
-        let passSale = !this.filterOptions.sale || plant.sale === this.filterOptions.sale;
+      return this.PLANTS.filter((plant) => {
+        let passCategory1 =
+          this.filterOptions.category1 === "" ||
+          plant.category1 === this.filterOptions.category1;
+        let passCategory2 =
+          this.filterOptions.category2 === "" ||
+          plant.category2 === this.filterOptions.category2;
+        let passAvailability =
+          this.filterOptions.availability === "" ||
+          plant.availability.toString() === this.filterOptions.availability;
+        let passSale =
+          !this.filterOptions.sale || plant.sale === this.filterOptions.sale;
         return passCategory1 && passCategory2 && passAvailability && passSale;
       });
-    }
+    },
   },
   mounted() {
     this.GET_PLANTS_FROM_API().then((response) => {
